18d831061872691d2f7b5dbb2e0e7232e88fc49b
Justin Clareburt <justcla@microsoft.com>
2017-02-05 04:04:11 -0800

    Created VS2017 mappings

---
+++ KeyboardSchemes/Installer/Build2017MSI.bat
@@ -0,0 +1,3 @@
+candle HotKeys2017.wxs
+light -ext WixVSExtension -ext WixUIExtension HotKeys2017.wixobj
+pause
--- KeyboardSchemes/Installer/BuildMSI.bat
+++
@@ -1,3 +0,0 @@
-candle HotKeys.wxs
-light -ext WixVSExtension -ext WixUIExtension HotKeys.wixobj
-pause
